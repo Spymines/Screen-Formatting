492bba3041fc470f919bcbaa7f2e732c4333383b
Trevor Mines <spymines@cox.net>
2020-10-15 18:08:21 -0400

    Started, added command line argument, working on file

---
+++ cobol/formatting.cob
@@ -0,0 +1,29 @@
+        IDENTIFICATION DIVISION.
+        PROGRAM-ID. formatting.
+        
+        ENVIRONMENT DIVISION.
+        INPUT-OUTPUT SECTION.
+        FILE-CONTROL.
+                SELECT file ASSIGN TO command1
+                        ORGANIZATION IS LINE SEQUENTIAL.
+
+        DATA DIVISION.
+        WORKING-STORAGE SECTION.
+        01 command1 PIC X(50).
+         
+        PROCEDURE DIVISION.
+
+        Begin.
+                ACCEPT command1 FROM ARGUMENT-VALUE
+                display command1
+
+                OPEN INPUT file
+                READ file
+                        AT END MOVE 1 TO END-OF-FILE
+                END READ
+                
+                IF END-OF-FILE = 1
+                        CLOSE file
+                END-IF
+
+        STOP RUN.
