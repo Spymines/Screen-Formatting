5cf8b2859d023b75480eef97a98a58962e63bac5
Trevor Mines <spymines@cox.net>
2020-10-29 17:19:26 -0400

    Worked on lisp, text read in working, need to remove extra spaces

--- lisp/formatting.lisp
+++ lisp/formatting.lisp
@@ -1,18 +1,43 @@
 #!/usr/bin/sbcl --script
 
 (defvar file)
-(defvar line)
+(defvar curr)
+(defvar lineCount)
 (setq lineCount 0)
+(defvar words)
+(defvar x 0)
+(defvar prev)
+(setq prev #\p)
+
+;(defvar input)
+;(setq input '())
+
+(defvar longString)
+
+(write-line "123456789*123456789*123456789*123456789*123456789*123456789*123456789*")
+
 
 (setq file (open (car(cdr *posix-argv* ) )))
 
-      (loop for line = (read-line file nil :eof) ; stream, no error, :eof value
-                  until (eq line :eof)
-                        do
-			(write-line line)
-			(incf lineCount)
-      )
+(setf longString(make-string(file-length file)))
 
-(print lineCount)
+      	(loop for curr = (read-char file nil :eof) ; stream, no error, :eof value
+                  until (eq curr :eof)
+                        do
+			(cond ((or (char> curr #\9) (char< curr #\0)) 
+				(cond ((char/= curr #\NewLine)
+					(setf (aref longString x) curr)
+					(incf x))
+					;Case 2       
+					((char= curr #\NewLine)
+					(setf (aref longString x) #\Space)
+					(incf x))
+				)	
+		  	      )
+			)
+      	)
+
+(print longString)
+;(print lineCount)
 (terpri)
 (close file)
