ad51e771be766050db88388f8aa55ada39aad6b0
Trevor Mines <spymines@cox.net>
2020-10-30 12:07:39 -0400

    File Input Completed

--- cobol/formatting.cob
+++ cobol/formatting.cob
@@ -4,13 +4,16 @@
         ENVIRONMENT DIVISION.
         INPUT-OUTPUT SECTION.
         FILE-CONTROL.
-                SELECT file ASSIGN TO command1
+               SELECT inputFile ASSIGN TO "../fortran/test.txt"
                         ORGANIZATION IS LINE SEQUENTIAL.
 
         DATA DIVISION.
-        01 generalInput PIC(256).
+        FILE SECTION.
+        FD inputFile.
+        01 currLine PIC X(256).
         WORKING-STORAGE SECTION.
         01 command1 PIC X(50).
+        01 END-OF-FILE PIC Z(1).
          
         PROCEDURE DIVISION.
 
@@ -18,13 +21,22 @@
                 ACCEPT command1 FROM ARGUMENT-VALUE
                 display command1
 
-                OPEN INPUT file
-                READ file
+                OPEN INPUT inputFile
+                READ inputFile
                         AT END MOVE 1 TO END-OF-FILE
-                END READ
-                
-                IF END-OF-FILE = 1
-                        CLOSE file
+                END-READ
+               
+                IF END-OF-FILE = 1 
+                        CLOSE inputFile
                 END-IF
 
+                MOVE 0 TO END-OF-FILE.
+
+                PERFORM UNTIL END-OF-FILE = 1
+                        DISPLAY currLine
+                        READ inputFile
+                                AT END MOVE 1 TO END-OF-FILE
+                        END-READ
+                END-PERFORM
+                CLOSE inputFile
         STOP RUN.
